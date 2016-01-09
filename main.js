a17d4716b952f5576a3417c8a1f6600cc2e76c0c
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-08 21:50:51 -0800

    we have movement

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
+var whatTower = "-1";
+
 $(document).ready(init);
 
 function init() {
-	$('.press').on('click', selFirstChild);
+	$('.tower').on('click', selFirstChild);
 }
 
 function selFirstChild() {
-	var $fChild = $(this).find('.rings:first-child');
 
-	if ($fChild.hasClass('selected')) {
-		$fChild.removeClass('selected');
+	if (whatTower === "-1") {
+		$tower = $(this);
+		whatTower = $tower.find('.press').data('val');
+		$val = $tower.find('.press').data('val');
+		//$ring = $this.find('.rings:first-child');
 	} else {
-		$fChild.addClass('selected');
+		$tower1 = $(this);
+		$val1 = $tower1.find('.press').data('val')
+		//$ring1 = $this.find('.rings:first-child');
+		if ($val !== $val1) {
+			console.log('leggo');
+			$ring = $tower.find('.rings:first-child');
+			$ring1 = $tower1.find('.rings:first-child');
+			console.log($ring);
+			console.log($ring1);
+			if ($ring.width() < $ring1.width()) {
+				console.log('leggo!!!');
+				var move = $ring.remove();
+				$tower1.find('.press').prepend(move);
+			}
+		}
+
+
 	}
+	
+
+
+
+
+
+
+	// if ($ring.hasClass('selected')) {
+	// 	$ring.removeClass('selected');
+	// 	whatTower = "-1";
+	// 	$ring = {};
+	// 	$this = {};
+	// } else {
+	// 	if (whatTower === "-1") {
+	// 		$ring.addClass('selected');
+	// 		whatTower = $this.find('.press').data('val');
+	// 	}
+	// }
+	// console.log(whatTower);
+	// console.log($this);
+	// console.log($ring);
 }
