d3b139b360aed623d32ffb6e94a276c432294428
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-09 12:48:10 -0800

    add pole, round rings

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var $tower;
 var $tower1;
 var $ring;
 var $ring1;
-var $val;
-var $val1;
+var $t1 = $('#tower1');
+var $t3 = $('#tower3');
 $(document).ready(init);
 
 function init() {
@@ -18,7 +18,6 @@ function selFirstChild() {
 		$tower = $(this);
 		whatTower = $tower.find('.rings').data('val');
 		$ring = $tower.find('.rings:first-child');
-		$val = $tower.find('.press').data('val');
 		if ($tower.find('.rings').length === 0) {
 			whatTower = "-1";
 		} else {
@@ -27,23 +26,21 @@ function selFirstChild() {
 	} else {
 		$tower1 = $(this);
 		$ring1 = $tower1.find('.rings:first-child');
-		$val1 = $tower1.find('.press').data('val');
 		$ring.removeClass('selected');
-		if ($val !== $val1) {
-			if ($ring.width() < $ring1.width() || $tower1.find('.rings').length === 0) {
-				moves++;
-				var move = $ring.remove();
-				$tower1.find('.press').prepend(move);
-				$('span').html(moves);
-				checkForWin();
-			}
+		if ($ring.width() < $ring1.width() || $tower1.find('.rings').length === 0) {
+			moves++;
+			var move = $ring.remove();
+			$tower1.find('.press').prepend(move);
+			$('span').html(moves);
+			checkForWin();
 		}
 		whatTower = "-1";
 	}
 }
 
 function checkForWin() {
-	if ($('#tower1').find('.rings').length === 4 || $('#tower3').find('.rings').length === 4) {
+	if ($t1.find('.rings').length === 4 || $t3.find('.rings').length === 4) {
 		$('#winner').html("WINNER");
 	}
 }
+
